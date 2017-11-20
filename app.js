532c846944535e3a5678853c6da0ce89518be285
xiadong8 <xiadong8@wanda.cn>
2017-11-20 18:48:46 +0800

    statistic

--- app.js
+++ app.js
@@ -8,10 +8,34 @@ Vue.component('goods', {
   template: '#tpl-goods',
   methods: {
     gotoDetail () {
-      window.location.href = this.goods.link
+      if (this.goods.link) window.location.href = this.goods.link
     }
   }
 });
+
+Vue.component('statistic', {
+  props: ['products'],
+  template: '#tpl-statistic',
+  computed: {
+    price () {
+      var r = 0
+      this.products.forEach(p => {
+        r += p.paid * p.count
+      })
+      return r.toFixed(2)
+    },
+    count () {
+      var n = 0
+      this.products.forEach(p => {
+        n = n + p.count * 1
+      })
+      return n
+    },
+    count2 () {
+      return 0
+    }
+  }
+})
 
 new Vue({
   el: '#app',
